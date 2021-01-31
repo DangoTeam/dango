c49fece9ff2553f32f0d889f26c5f14ef3bb382f
shaundeveloper <mastercanguro@hotmail.com>
2021-01-31 20:08:01 -0300

    Refatoring

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,42 @@
-use std::process::{ Command, Stdio, Child };
-use std::io::{ stdout, stdin, Write };
-use std::path::Path;
 use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
+    let mut input = String::new();
+
     loop {
         print!("$ ");
         stdout().flush().ok();
 
-        let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
         let mut commands = input.trim().split(" | ").peekable();
         let mut previous_command = None;
 
         while let Some(command) = commands.next() {
-            let mut parts = command.trim().split_whitespace();
-            let command = parts.next().unwrap();
-            let args = parts;
+            let mut args = command.trim().split_whitespace();
+            let command = args.next().unwrap();
 
             match command {
                 "cd" => {
-                    let new_dir = args.peekable().peek()
-                        .map_or("/", |x| *x);
+                    let new_dir = args.next().unwrap_or_else("/");
                     let root = Path::new(new_dir);
                     if let Err(e) = env::set_current_dir(&root) {
                         eprintln!("{}", e);
                     }
 
                     previous_command = None;
-                },
+                }
 
                 "exit" => return,
 
                 command => {
-                    let stdin = previous_command
-                        .map_or(
-                            Stdio::inherit(),
-                            |output: Child| Stdio::from(output.stdout.unwrap())
-                        );
-                    
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
                     let stdout = if commands.peek().is_some() {
                         Stdio::piped()
                     } else {
@@ -51,13 +48,15 @@ fn main() {
                         .stdin(stdin)
                         .stdout(stdout)
                         .spawn();
-    
+
                     match output {
-                        Ok(output) => { previous_command = Some(output); },
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
                         Err(_e) => {
                             previous_command = None;
                             eprintln!("dango: command not found: {}", command);
-                        },
+                        }
                     };
                 }
             }
